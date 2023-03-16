d5f661f8602dfb519c869d06dad278114a72b39e
iskandarovakmal <aiskandarov070707@gmail.com>
2023-03-16 15:02:52 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+function App() {
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className='container'>
+        <h3>Savol va javoblar OWA o'quv markazi haqida</h3>
+        <section className='info'>
+          {questions.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question}></SingleQuestion>
+            );
+          })}
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,28 @@
+const questions = [
+	{
+		id: 1,
+		title: 'Kursni tugatgandan song sertifikat beriladimi?',
+		info:
+			'Biz faqatgina kurismizni imtihonlaridan muvaffaqiyatli bitirgan oquvchilarimizgagina sertifikat beramiz.'
+	},
+	{
+		id: 2,
+		title: 'Kursni tugatganimdan song ish bilan Taminlanamanmi?',
+		info:
+			'Biz kursni muvaffaqiyatli tugatgan oquvchilarimizni ishga kirish jarayonigacha yordam beramiz va hamkorlarimizga tavsiya qilamiz.Lekin hech qaysi oquvchimizga ish bilan taminlaymiz deb vada bermaymiz.Bu jarayon har bir oquvchining potensialiga ham bogliq hisoblanadi.'
+	},
+	{
+		id: 3,
+		title: 'Kurs boyicha taklif va shikoyatlarim bor.Qayerga murojaat qilsam buladi?',
+		info:
+			'Agar sizda kurs buyicha taklif va tavsiyalar,shikoyatlar yoki murojaat bolsa, +998 75 2200107 telefon raqamiga qongiroq qilishingiz mumkin.'
+	},
+	{
+		id: 4,
+		title: 'Kurslar uchun qanday chegirmalar mavjud?',
+		info:
+			'Bir oiladan 2 nafar yoki undan ortiq oquvchi boshlasa, har bir oquvchi uchun kurs narxidan 10% chegirma qilib beramiz.'
+	},
+	
+];
+export default questions;
